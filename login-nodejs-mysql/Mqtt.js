a10bcdd1f51624e43bbbcb2446668a7cb871fb0f
Dhaval2908 <dhavalkhunt17@gmail.com>
2022-03-04 15:45:07 +0530

    LIVEDATA,FIREBASE LOGIN,SIGNUP,PASSWORD RESET MAIL

--- login-nodejs-mysql/Mqtt.js
+++ login-nodejs-mysql/Mqtt.js
@@ -22,6 +22,9 @@ client.on('connect', () => {
   client.subscribe("REEVA/HYDROPHONICS/34B4724F22C4/DHT12/Temp", () => {
     console.log(`Subscribe to topic '${topic}'`)
   })
+  client.subscribe("TEST", () => {
+    console.log(`Subscribe to topic '${topic}'`)
+  })
   client.subscribe("REEVA/HYDROPHONICS/34B4724F22C4/DHT12/Humidity", () => {
     console.log(`Subscribe to topic '${topic}'`)
   })
@@ -30,7 +33,7 @@ client.on('connect', () => {
 var flag = 0
 var time = 0
 var date = 0
-cron.schedule('0 */5 * * * *', () => {
+cron.schedule(' */1 * * * *', () => {
   console.log("Calling")
   //  '0 */5 * * * *
   getdata();
@@ -44,51 +47,38 @@ function getdata() {
       console.error(error)
     }
   })
-
 }
 
 client.on('message', (topic, payload) => {
-
-  console.log(payload.toString())
   if (topic === "REEVA/HYDROPHONICS/34B4724F22C4/DHT12/Temp") {
     Temp = payload.toString();
-  
+    console.log("TEMP",Temp)
+
   }
   else if (topic === "REEVA/HYDROPHONICS/34B4724F22C4/DHT12/Humidity") {
     humidity = payload.toString();
-    console.log(humidity)
+    console.log("Humidity",humidity)
+    savedata(Temp, humidity)
   }
-  setTimeout(() => {
-    savedata(Temp,humidity)
-  }, 100);
-
- 
-
+;
 })
 
-
-
-function savedata(Temp, humidity, Pump) {
+function savedata(Temp, humidity) {
   var date_ob = new Date();
   flag = 0
-  console.log("humidity", humidity)
-  if (humidity === 'NULL') {
-    humidity = '5.5'
-  }
   let day = ("0" + date_ob.getDate()).slice(-2);
   let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
   let minute = String(date_ob.getMinutes()).padStart(2, '0');
   time = date_ob.getHours() + ':' + minute + ':' + date_ob.getSeconds();
   date = date_ob.getFullYear() + '/' + month + '/' + day;
-  console.log(time)
-  console.log(date)
+
   var t2 = "53"
   var t3 = "54"
-  console.log(Temp)
+
   if (Temp > 28) {
     Fan = "ON"
-    console.log(Fan)
-    client.publish("REEVA/HYDROPHONICS/34B472504B4C/C/2", "ON:100", { qos: 0, retain: false }, (error) => {
+
+    client.publish("REEVA/HYDROPHONICS/34B472504B4C/C/1", "ON:100", { qos: 0, retain: false }, (error) => {
       if (error) {
         console.error(error)
       }
@@ -96,28 +86,27 @@ function savedata(Temp, humidity, Pump) {
   }
   else {
     Fan = "OFF"
-    client.publish("REEVA/HYDROPHONICS/34B472504B4C/C/2", "OFF:0", { qos: 0, retain: false }, (error) => {
+    client.publish("REEVA/HYDROPHONICS/34B472504B4C/C/1", "OFF:0", { qos: 0, retain: false }, (error) => {
       if (error) {
         console.error(error)
       }
     })
   }
-  //Temp="22.0"
-  console.log(Fan)
-  var sql = "INSERT INTO data (Temp,Humidity,EC,PH,Time,Date,Fan) VALUES (?,?,?,?,?,?,?);"
-  con.query(sql, [Temp, humidity, t2, t3, time, date, Fan], function (err, result) {
-    if (err) throw err;
-    console.log("1 record inserted");
-  });
 
+  console.log(time)
+  //Temp="22.0"
+  
+  // var sql = "INSERT INTO data (Temp,Humidity,EC,PH,Time,Date,Fan) VALUES (?,?,?,?,?,?,?);"
+  // con.query(sql, [Temp, humidity, t2, t3, time, date, Fan], function (err, result) {
+  //   if (err) throw err;
+  //   console.log("1 record inserted");
+  // });
   // const fileData = fs.readFileSync("data.json", 'utf8');
   // const object = JSON.parse(fileData)
-  fs.writeFileSync("data.json", JSON.stringify([{Temp:Temp,Humidity:humidity,EC:t2,PH:t3,Time:time,Date:date,Fan:Fan}], null, 2))
-
-
+  fs.writeFileSync("data.json", JSON.stringify([{ Temp: Temp, Humidity: humidity, EC: t2, PH: t3, Time: time, Date: date, Fan: Fan }], null, 2));
   time = 0
   Fan = ""
-  console.log(time)
+
 }
 
 
--- login-nodejs-mysql/login.js
+++ login-nodejs-mysql/login.js
@@ -1,12 +1,22 @@
 const mysql = require("mysql");
 const express = require("express");
 const bodyParser = require("body-parser");
-const encoder = bodyParser.urlencoded();
+ const encoder = bodyParser.urlencoded();
 const logger = require("morgan")
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var jwt = require('jsonwebtoken');
 var fs = require('fs');
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+
 
 const app = express();
 app.use(cookieParser());
@@ -15,348 +25,223 @@ app.use(logger("dev"))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use("/assets", express.static("assets"));
+app.engine("html", require("ejs").renderFile);
 
-const schedule = require("./schedule")
+
+const test = require("./schedule")
 const connection = require("./database");
 const client = require("./Mqtt");
 const { Console } = require("console");
 
-app.get("/", function (req, res) {
-    res.redirect('/index1');
+app.get("/",isLoggedIn, function (req, res) {
+    res.redirect('login');
 })
 
-app.get('/test', (req, res) => {
-    res.render('test', {
-        message: ''
-    });
+app.get('/signup', (req, res) => {
+    res.render("signup.html");
 });
-app.get('/index1', (req, res) => {
-    res.render('index1', {
-        message: ''
-    });
+app.get('/login',LOGIN, (req, res) => {
+    res.render('login');
 });
+app.get("/dashboard",isLoggedIn,(req,res)=>{
+  client.publish("REEVA/HYDROPHONICS/34B4724F22C4/Action", "1", { qos: 0, retain: false }, (error) => {
+    if (error) {
+        console.error(error)
+    }
 
-
-app.post("/", function (req, res) {
-    var username = req.body.username;
-    var password = req.body.password;
-
-    connection.query("select * from test where username = ? ", [username], function (error, results, fields) {
-        console.log(results)
-
-        if (results.length > 0) {
-            results.forEach(data => {
-                if (data.password == password) {
-                    const token = jwt.sign({
-                        email: data.username,
-                    }, 'test secret', { expiresIn: '3h' });
-                    // Set session expiration to 3 hr.
-                    console.log(token)
-                    const expiresIn = 60 * 60 * 3 * 1000;
-                    console.log(60 * 60 * 3 * 1000)
-                    const options = { maxAge: 60 * 60 * 3 * 1000, httpOnly: true };
-                    res.cookie('token', token, options);
-                    // res.cookie('id', user[0].id, options);
-                    res.redirect("/dashboard")
-                }
-                else {
-                    res.render("index1", {
-                        message: "Password Does Not Match"
-                    });
-                }
-            });
-        }
-        else {
-            res.render("index1", {
-                message: "User Does not exist"
-            });
-        }
-
-
-
-    })
-})
-app.post("/test", function (req, res) {
-    var username = req.body.username;
-    var password = req.body.password;
-    var repassword = req.body.repassword;
-    connection.query("select * from test where username = ? ", [username], function (error, results, fields) {
-        if (results.length > 0) {
-            res.render("test", {
-                message: "User is already exist"
-            });
-        }
-        else {
-            if (password == repassword) {
-                connection.query("INSERT INTO test (username, password) VALUES (?, ?);", [username, password], function (error, results, fields) {
-                    if (results.length > 0) {
-                        res.render("/")
-
-                    }
-                    else {
-                        res.redirect("/");
-
-                    }
-                    res.end();
-                })
-            }
-            else {
-                res.render("test", {
-                    message: "Password Does Not Match"
-                });
-            }
-        }
-
-    })
-
-
-})
-app.get("/dashboard", isLoggedIn, function (req, res) {
-    // var sql = 'SELECT * FROM data ORDER BY Date DESC , Time DESC Limit 1';
-    // connection.query(sql, function (err, d, fields) {
-    //     console.log(d)
-    //     data = d
-    const fileData = fs.readFileSync("data.json", 'utf8');
+}) 
+setTimeout(() => {
+    const data = fs.readFileSync("data.json", 'utf8');
+    const DATA = JSON.parse(data)
+    const fileData = fs.readFileSync("status.json", 'utf8');
     const object = JSON.parse(fileData)
+    res.render("dashboard", {
+        Data : DATA,
+        Status: object
+    });
+}, 500);
+})
+app.post("/dashboard", encoder,isLoggedIn ,function (req, res) {
+  var AirPump = req.body.AirPump;
+  console.log(AirPump)
+  
+  client.publish("REEVA/HYDROPHONICS/34B4724F22C4/Action", "1", { qos: 0, retain: false }, (error) => {
+      if (error) {
+          console.error(error)
+      }
 
-        res.render("dashboard", {
-            userData: object,
-            status:"OFF"
-        });
-
-    })
-
-// })
-
-app.post("/dashboard", function (req, res) {
-    var AirPumpON = req.body.AirPumpON;
-    var AirPumpOFF = req.body.AirPumpOFF;
-    var WaterPumpON = req.body.WaterPumpON;
-    var WaterPumpOFF = req.body.WaterPumpOFF;
-    var LightON = req.body.LightON;
-    var LightOFF = req.body.LightOFF;
-
-    client.publish("REEVA/HYDROPHONICS/34B4724F22C4/Action", "1", { qos: 0, retain: false }, (error) => {
-        if (error) {
-            console.error(error)
-        }
-
-    })
-    setTimeout(function () {
-        console.log(AirPumpON)
-        if (AirPumpON) {
-            console.log("t")
-           let  s="ON"
-            client.publish("REEVA/HYDROPHONICS/34B472504B4C/C/1", "ON:100", { qos: 0, retain: false }, (error) => {
-                if (error) {
-                    console.error(error)
-                }
-            })
-
-        }
-        if (AirPumpOFF) {
-            let  s="OFF"
-            client.publish("REEVA/HYDROPHONICS/34B472504B4C/C/1", "OFF:0", { qos: 0, retain: false }, (error) => {
-                if (error) {
-                    console.error(error)
-                }
-            })
-        }
-        console.log(AirPumpON)
-        if (WaterPumpON) {
-            console.log("t")
-            client.publish("REEVA/HYDROPHONICS/34B472504B4C/C/3", "ON:100", { qos: 0, retain: false }, (error) => {
-                if (error) {
-                    console.error(error)
-                }
-            })
-
-        }
-        if (WaterPumpOFF) {
-            client.publish("REEVA/HYDROPHONICS/34B472504B4C/C/3", "OFF:0", { qos: 0, retain: false }, (error) => {
-                if (error) {
-                    console.error(error)
-                }
-            })
-        }
-
-        if (LightON) {
-            console.log("t")
-            client.publish("REEVA/HYDROPHONICS/34B472504B4C/C/5", "ON:100", { qos: 0, retain: false }, (error) => {
-                if (error) {
-                    console.error(error)
-                }
-            })
-
-        }
-        if (LightOFF) {
-            client.publish("REEVA/HYDROPHONICS/34B472504B4C/C/5", "OFF:0", { qos: 0, retain: false }, (error) => {
-                if (error) {
-                    console.error(error)
-                }
-            })
-        }
-        // var sql = 'SELECT * FROM data ORDER BY Date DESC , Time DESC Limit 1';
-        // connection.query(sql, function (err, d, fields) {
-        //     console.log(d)
-        //     data = d
-
-
-        //     res.render("dashboard", {
-        //         userData: data,
-        //     });
-
-        // })
-        const fileData = fs.readFileSync("data.json", 'utf8');
-        const object = JSON.parse(fileData)
-    
-            res.render("dashboard", {
-                userData: object,
-                status:s
-            });
-    
-       
-    }, 500);
-
-
+  })      
+      if(AirPump=="ON")
+      {
+         
+          client.publish("REEVA/HYDROPHONICS/34B472504B4C/C/2", "ON:100", { qos: 0, retain: false }, (error) => {
+              
+              if (error) {
+                  console.error(error)
+              }
+          })
+          const fileData = fs.readFileSync("status.json", 'utf8');
+          const object = JSON.parse(fileData)
+          fs.writeFileSync("status.json", JSON.stringify({ Pump: "ON" }, null, 2))
+      }
+      if(AirPump=="OFF")
+      {
+         
+          client.publish("REEVA/HYDROPHONICS/34B472504B4C/C/2", "OFF:0", { qos: 0, retain: false }, (error) => {
+              
+              if (error) {
+                  console.error(error)
+              }
+          })
+          const fileData = fs.readFileSync("status.json", 'utf8');
+          const object = JSON.parse(fileData)
+          fs.writeFileSync("status.json", JSON.stringify({ Pump: "OFF" }, null, 2))
+      }
+     
+      setTimeout(() => {
+      const data = fs.readFileSync("data.json", 'utf8');
+      const DATA = JSON.parse(data)
+      const fileData = fs.readFileSync("status.json", 'utf8');
+      const object = JSON.parse(fileData)
+      res.render("dashboard", {
+          Data : DATA,
+          Status: object
+      });
+      }, 500);
+      
+  
 
 })
-app.get("/schedule", isLoggedIn, function (req, res) {
+app.post("/sessionLogin", (req, res) => {
+    const idToken = req.body.idToken.toString();
+    const expiresIn = 60* 60*1* 1000;
+    console.log(expiresIn)
+  
+    admin
+      .auth()
+      .createSessionCookie(idToken, { expiresIn })
+      .then(
+        (sessionCookie) => {
+          const options = { maxAge: expiresIn, httpOnly: true };
+          res.cookie("session", sessionCookie, options);
+          res.end(JSON.stringify({ status: "success" }));
+        },
+        (error) => {
+          res.status(401).send("UNAUTHORIZED REQUEST!");
+        }
+      );
+  });
 
-
+  app.get("/logout", (req, res) => {
+    res.clearCookie("session");
+    res.redirect("/login");
+  });
+  app.get("/resetpassword",(req,res)=>{
+    res.render("resetpassword.html")
+   })
+   app.get("/schedule", isLoggedIn, function (req, res) {
     const fileData = fs.readFileSync("schedule.json", 'utf8');
-    console.log("LEngth", fileData.length)
-    // if(fileData.length === 0)
-    // {
-    //     console.log("f")
-    //    fs.writeFileSync("schedule.json", JSON.stringify([], null, 2))
-
-    // }
-    // setTimeout(function () {
-
-    //     const object = JSON.parse(fileData)
-    //     res.render("schedule", {
-    //         ScheduleData: object
-    //     });
-    // }, 10);
     const object = JSON.parse(fileData)
 
     res.render("schedule", {
         ScheduleData: object
     });
-
-
-
-
-
 })
+test()
 
 app.post("/schedule", isLoggedIn, function (req, res) {
     StartTime = req.body.starttime
     EndTime = req.body.endtime
-
     StartTime = StartTime.split(":");
     EndTime = EndTime.split(":");
-    valid = parseInt(StartTime[0]) + parseInt(StartTime[1]) - parseInt(EndTime[0]) - parseInt(EndTime[1])
-    console.log("Valid:", valid)
-    if (valid >= 0) {
-        console.log("ERROR")
-    }
     var date_ob = new Date();
     let minute = String(date_ob.getMinutes()).padStart(2, '0');
     time = date_ob.getHours() + ':' + minute;
     const fileData = fs.readFileSync("schedule.json", 'utf8');
-
     const object = JSON.parse(fileData)
     if (object.length === 0) {
 
-        fs.writeFileSync("schedule.json", JSON.stringify([{ StartHr: StartTime[0], StartMin: StartTime[1], EndHr: EndTime[0], EndMin: EndTime[1] }], null, 2))
+        fs.writeFileSync("schedule.json", JSON.stringify([{ StartHr: StartTime[0], StartMin: StartTime[1], StartSec: StartTime[2], EndHr: EndTime[0], EndMin: EndTime[1], EndSec: EndTime[2] }], null, 2))
+        client.publish("TEST", "1", { qos: 0, retain: false }, (error) => {
+            if (error) {
+                console.error(error)
+            }
+        })
     } else {
 
-        object.push({ StartHr: StartTime[0], StartMin: StartTime[1], EndHr: EndTime[0], EndMin: EndTime[1] })
+        object.push({ StartHr: StartTime[0], StartMin: StartTime[1], StartSec: StartTime[2], EndHr: EndTime[0], EndMin: EndTime[1], EndSec: EndTime[2] })
         fs.writeFileSync("schedule.json", JSON.stringify(object, null, 2))
+        client.publish("TEST", "1", { qos: 0, retain: false }, (error) => {
+            if (error) {
+                console.error(error)
+            }
+        })
+
 
     }
-    // obj.table.push({StartTime:StartTime,EndTime:EndTime})
-
-    // fs.appendFileSync('schedule.json', JSON.stringify(obj)  ,(err) => {
-    //     if (err)
-    //       console.log(err);
-    //     else {
-    //       console.log("File written successfully\n");
-    //       console.log("The written has the following contents:");
-
-    //     }
-    // })
-    // setTimeout(function () {
-    //     res.render("schedule",{
-    //         ScheduleData:object
-    //     });
-    // }, 500);
     res.redirect("/schedule")
-
-
-
-
 })
-function isLoggedIn(req, res, next) {   //To verify an incoming token from client
-    console.log(req.cookies.token);
-    try {
-        console.log(req.cookies.token);
-        jwt.verify(req.cookies.token, 'test secret');
-        return next();
+client.on('message', (topic, payload) => {
+    if (topic === "TEST") {
+        test()
     }
-    catch (err) {
-        console.log(err.message);
-        return res.status(401).render('index1', {  //401 Unauthorized Accesss
-            message: 'Please Login Again'
-        });
-    }
-}
+})
 app.post("/delete", isLoggedIn, function (req, res) {
-
     const fileData = fs.readFileSync(`schedule.json`, 'utf8');
     const object = JSON.parse(fileData)
-
     index = object.findIndex(object => object.StartHr == req.query.StartHr && object.StartMin == req.query.StartMin)
-
     object.splice(index, 1)
     fs.writeFileSync("schedule.json", JSON.stringify(object, null, 2))
-
-
+    client.publish("TEST", "1", { qos: 0, retain: false }, (error) => {
+        if (error) {
+            console.error(error)
+        }
+    })
     res.redirect("/schedule")
-
-
-
-
 })
-// setInterval(() => {
-//     console.log("StartTime:",StartTime)
-//     console.log("EndTime:",EndTime)
-//     Temp=StartTime
-//     Temp2=EndTime
-//     if (StartTime) {
-//         Temp = Temp.split(":");
-//         Temp2 = Temp2.split(":");
-//         var date_ob = new Date();
-//         let minute = String(date_ob.getMinutes()).padStart(2, '0');
+app.get("/analysis", isLoggedIn, function (req, res) {
+    
+  connection.query("select * from data", function (error, results, fields) {
+      console.log(results)
+      res.render("analysis",{
+          red :results
+          });
+  })
+  
+})
 
 
-//         diff = parseInt(Temp[0]) + parseInt(Temp[1]) - parseInt(minute) - date_ob.getHours()
-//         diff2 = parseInt(Temp2[0]) + parseInt(Temp2[1]) - parseInt(minute) - date_ob.getHours()
-//         console.log(diff2)
-//         if (diff > 0 || diff2 <= 0) {
-//             console.log("OFF")
-//         }
-//         else {
-//             console.log("ON")
-//         }
-
-//     }
-//     },10000)
-
-
+  function isLoggedIn(req, res, next) {   //To verify an incoming token from client
+    const sessionCookie = req.cookies.session || "";
+   
+    admin
+      .auth()
+      .verifySessionCookie(sessionCookie, true /** checkRevoked */)
+      .then((userData) => {
+        console.log("Logged in:", userData.email)
+        next()
+      })
+      .catch((error) => {
+        console.log("error::::::",error)
+        res.redirect("/login");
+      });
+    
+  }
+  function LOGIN(req, res, next) {   //To verify an incoming token from client
+    const sessionCookie = req.cookies.session || "";
+   
+    admin
+      .auth()
+      .verifySessionCookie(sessionCookie, true /** checkRevoked */)
+      .then((userData) => {
+        console.log("Logged in:", userData.email)
+        res.redirect("/dashboard")
+      })
+      .catch((error) => {
+        res.render("login");
+      });
+  }
+ 
 
 app.listen(4000, () => {
     console.log(`server started 4000`)
